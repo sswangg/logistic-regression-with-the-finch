ba2fecf3631b5d08d6cddd2bd03efc12438ca411
Saph <s.sophi.w@gmail.com>
2021-09-27 11:04:46 -0400

    added finch stuff

--- generate_training_data.py
+++ generate_training_data.py
@@ -1,9 +1,20 @@
 import random
 
 c = 0
-while c < 400:
+
+
+while c < 50:
     a = random.uniform(-1, 1)
     b = random.uniform(-1, 1)
-    if not (a > 0.6 and abs(a - b) < 0.1):
+    if a > 0.6 and abs(a - b) < 0.05:
         print(a, b)
         c += 1
+"""
+
+while c < 450:
+    a = random.uniform(-1, 1)
+    b = random.uniform(-1, 1)
+    if not (a > 0.6 and abs(a - b) < 0.05):
+        print(a, b)
+        c += 1
+"""
--- ml_test.py
+++ ml_test.py
@@ -2,16 +2,15 @@ import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.linear_model import LogisticRegression
 from finch import Finch
+from random import uniform
+from time import sleep
 from sklearn.metrics import classification_report, confusion_matrix
 
-"""
-x = np.arange(10).reshape(-1, 1)
-y = np.array([0, 0, 0, 0, 1, 1, 1, 1, 1, 1])
+# Finch has two modes, learn and predict
+# In learn, the finch sets its wheels to the data points that it's most confused about to reduce confusion
+# In predict, the finch tries to go forwards using current data
 
-model = LogisticRegression(solver='liblinear', random_state=0)
-model.fit(x, y)
-print(model.predict(x))
-"""
+finch = Finch()
 
 
 def data_to_scatter(file, color):
@@ -33,17 +32,27 @@ data_to_scatter("other_data.txt", "blue")
 plt.show()
 
 f_in = open("forward_data.txt", "r")
-data = [[float(a) for a in s.split()] for s in f_in.readlines()]
+data1 = [[float(a) for a in s.split()] for s in f_in.readlines()]
 f_in.close()
 f_in = open("new_other_data.txt", "r")
-data += [[float(a) for a in s.split()] for s in f_in.readlines()]
+data2 = [[float(a) for a in s.split()] for s in f_in.readlines()]
 f_in.close()
+data = data1+data2
 
-# All wheel speed data, 100 that go forwards then 1000 that don't
+# All wheel speed data
 wheel_data_x = np.array([a for l in data for a in l]).reshape(-1, 2)
-# 100 1s for going forwards, 1000 0s for not
-wheel_data_y = np.array([1 for i in range(100)]+[0 for j in range(400)])
+# 1s for going forwards, 0s for not
+wheel_data_y = np.array([1 for i in data1]+[0 for j in data2])
 
 model = LogisticRegression(solver='liblinear', random_state=0)
 model.fit(wheel_data_x, wheel_data_y)
 print(model.predict(wheel_data_x))
+
+zAccel = finch.acceleration()[2]
+
+while zAccel > -0.7:
+    w = [uniform(-1, 1), uniform(-1, 1)]
+    if model.predict(np.array(w).reshape(-1, 2)) > 0.7:
+        finch.wheels(*w)
+        sleep(1.0)
+    zAccel = finch.acceleration()[2]
