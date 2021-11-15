45fb47e97ca8ab121ac53c5e6df013c2ef207c0f
Saph <s.sophi.w@gmail.com>
2021-11-15 11:01:52 -0500

    Added comments

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ from learn import learn
 from predict import predict
 from finch import Finch
 
+# Finch has two modes, learn and predict
+# In learn, the finch sets its wheels to the data points that it's most confused about to reduce confusion
+# In predict, the finch tries to go forwards using current data
+
 f_cor = input("Please enter the name of the file you want to save correct data to: ")
 f_incor = input("Please enter the name of the file you want to save incorrect data to: ")
 
